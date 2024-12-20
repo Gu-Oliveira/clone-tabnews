59fa69d72260f40c0a12f3b8879243e08db922d0
Gustavo Oliveira <87765086+Gu-Oliveira@users.noreply.github.com>
2024-12-20 23:00:30 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Reyd e Anne, vocês são tudo pra mim. Amo vocês! ❤️</h1>
+  return <h1>Reyd e Anne, vocês são tudo pra mim. Amo vocês! ❤️</h1>;
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
